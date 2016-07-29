8807aad8c7280b569b476fa208f32afadec6b391
sean-tan-columbia <sean.tan.columbia@gmail.com>
2016-07-29 02:05:08 -0400

    Update DemoVerticle.java

--- server/src/main/java/com/mycloset/server/verticle/DemoVerticle.java
+++ server/src/main/java/com/mycloset/server/verticle/DemoVerticle.java
@@ -1,12 +1,21 @@
 package com.mycloset.server.verticle;
 
+import com.apple.eawt.AppEvent;
 import com.mycloset.server.model.Client;
 import com.mycloset.server.util.Runner;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by jtan on 7/28/16.
@@ -23,6 +32,14 @@ public class DemoVerticle extends AbstractVerticle {
         // Create a router object.
         Router router = Router.router(vertx);
 
+        JsonObject config = new JsonObject()
+                .put("url", "jdbc:mariadb://104.196.15.12:3306/test?autoReconnect=true")
+                .put("driver_class", "org.mariadb.jdbc.Driver")
+                .put("max_pool_size", 30)
+                .put("user", "tajinx")
+                .put("password", "jtan");
+        JDBCClient jdbcClient = JDBCClient.createShared(vertx, config);
+
         // Bind "/" to our hello message - so we are still compatible.
         router.route("/").handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
@@ -30,12 +47,29 @@ public class DemoVerticle extends AbstractVerticle {
                     .end("<h1>Hello from my first Vert.x 3 application</h1>");
         });
 
-        router.get("/api/client").handler(routingContext -> {
-            System.out.println("ClientId: " + routingContext.request().getParam("id"));
-            Client client = getClient(Long.parseLong(routingContext.request().getParam("id")));
-            routingContext.response()
-                    .putHeader("content-type", "application/json; charset=utf-8")
-                    .end(Json.encodePrettily(client));
+        jdbcClient.getConnection(res -> {
+            if (res.succeeded()) {
+                System.out.println("[Info] Connected to database.");
+                SQLConnection connection = res.result();
+                router.get("/api/client").handler(routingContext -> {
+                    String clientId = routingContext.request().getParam("id");
+                    connection.queryWithParams("SELECT * FROM test.client WHERE id = ?", new JsonArray().add(clientId), res2 -> {
+                        if (res2.failed()) {
+                            System.err.println("Cannot retrieve the data from the database");
+                            res2.cause().printStackTrace();
+                            return;
+                        }
+                        List<String> results = res2.result().getResults().stream()
+                                .map(JsonArray::encode)
+                                .collect(Collectors.toList());
+                        routingContext.response()
+                                .putHeader("content-type", "application/json; charset=utf-8")
+                                .end(Json.encodePrettily(results));
+                    });
+                });
+            } else {
+                System.out.println("[Error] Failed to connect to database.");
+            }
         });
 
         // Create the HTTP server and pass the "accept" method to the request handler.
@@ -50,18 +84,6 @@ public class DemoVerticle extends AbstractVerticle {
                             }
                         }
                 );
-    }
-
-    private Client getClient(long id) {
-        Client client = new Client(id);
-        client.setSsn("123-34-5678");
-        client.setName("Adam Smith");
-        client.setSex("M");
-        client.setBirthday("1970-02-28");
-        client.setNationality("USA");
-        client.setAddress("40 Lexington St.");
-        client.setEnrollDate("2015-09-30");
-        return client;
     }
 
 }
