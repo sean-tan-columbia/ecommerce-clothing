53e640407077e19429b45cb46662dda532f672ea
sean-tan-columbia <sean.tan.columbia@gmail.com>
2016-07-29 00:53:00 -0400

    Create Client.java

---
+++ server/src/main/java/com/mycloset/server/model/Client.java
@@ -0,0 +1,90 @@
+package com.mycloset.server.model;
+
+/**
+ * Created by jtan on 7/28/16.
+ */
+public class Client {
+
+    public Client(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEnrollDate() {
+        return enrollDate;
+    }
+
+    public void setEnrollDate(String enrollDate) {
+        this.enrollDate = enrollDate;
+    }
+
+    private long id;
+    private String ssn;
+    private String name;
+    private String sex;
+    private String nationality;
+    private String birthday;
+    private String occupation;
+    private String address;
+    private String enrollDate;
+
+}
