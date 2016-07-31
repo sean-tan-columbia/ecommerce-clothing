50c0d9bff4a13931dbdcfdb98bb0b0a187bf4cef
sean-tan-columbia <sean.tan.columbia@gmail.com>
2016-07-31 18:17:38 -0400

    Update DemoVerticle.java

--- server/src/main/java/com/mycloset/server/verticle/DemoVerticle.java
+++ server/src/main/java/com/mycloset/server/verticle/DemoVerticle.java
@@ -17,12 +17,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.Random;
+
 /**
  * Created by jtan on 7/28/16.
  */
 public class DemoVerticle extends AbstractVerticle {
 
-    private final static Logger logger = LoggerFactory.getLogger(DemoVerticle.class);
+    private final static Logger logger = LoggerFactory.getLogger("Server.Security.Activity.Monitor");
 
     public static void main(String[] args) {
         Runner.runExample(DemoVerticle.class);
@@ -49,21 +51,35 @@ public class DemoVerticle extends AbstractVerticle {
                     .end("<h1>Hello from my first Vert.x 3 application</h1>");
         });
 
+        List<String> ips = new ArrayList<String>();
+        ips.add("102.152.10.10");
+        ips.add("102.152.10.10");
+        ips.add("102.152.10.10");
+        ips.add("102.152.10.10");
+        ips.add("72.64.62.73");
+        ips.add("102.152.10.12");
+        ips.add("102.152.10.12");
+        ips.add("102.152.10.16");
+        ips.add("102.152.10.16");
+        ips.add("110.91.23.65");
+
         router.get("/api/client").handler(routingContext -> {
             String clientId = routingContext.request().getParam("id");
-            logger.info("Request for client " + clientId);
+            Random generator = new Random();
+            int randomIndex = generator.nextInt(10);
+            logger.info("Request from " + ips.get(randomIndex) + " for client ID: " + clientId);
             jdbcClient.getConnection(res -> {
                 if (res.succeeded()) {
                     SQLConnection connection = res.result();
                     // connection.queryWithParams("SELECT * FROM test.client WHERE id = ?", new JsonArray().add(clientId), res2 -> {
                     connection.query("SELECT * FROM test.client WHERE id=" + clientId, res2 -> {
                         if (res2.failed()) {
-                            logger.error("Cannot retrieve the data from the database");
+                            // logger.error("Cannot retrieve the data from the database");
                             routingContext.response()
                                     .putHeader("content-type", "application/json; charset=utf-8")
                                     .end(Json.encodePrettily(new ArrayList<>()));
                             connection.close();
-                            res2.cause().printStackTrace();
+                            //res2.cause().printStackTrace();
                             return;
                         }
                         List<String> results = res2.result().getResults().stream()
